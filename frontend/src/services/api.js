553ba310c5736be7bf4cb2cc09081c000c955d42
Kartikeya <kartikeya.anjul@gmail.com>
2026-05-19 05:29:39 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"http://localhost:8000/api",
+  baseURL:"https://cgq4646h-8000.inc1.devtunnels.ms/api",
 });
 
 export default api;
